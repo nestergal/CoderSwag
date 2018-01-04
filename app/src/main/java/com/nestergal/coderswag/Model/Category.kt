e29cae4b98fe8e761ee642b487a511f2565fbcaf
nestergal <nestergal@gmail.com>
2018-01-04 21:24:35 +0200

    lesson-02

--- app/src/main/java/com/nestergal/coderswag/Model/Category.kt
+++ app/src/main/java/com/nestergal/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.nestergal.coderswag.Model
 /**
  * Created by neste on 1/4/2018.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+            return title
+        }
+    }
