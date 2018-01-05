08de28308693752e3072daeb524219f4b04890b8
nestergal <nestergal@gmail.com>
2018-01-05 21:31:50 +0200

    lesson-04

---
+++ app/src/main/java/com/nestergal/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,66 @@
+package com.nestergal.coderswag.Adapters
+
+import android.content.Context
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.nestergal.coderswag.Model.Category
+import com.nestergal.coderswag.R
+
+/**
+ * Created by neste on 1/4/2018.
+ */
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+    val context = context
+    val categories = categories
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View
+
+        val holder : ViewHolder
+
+        if (convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+          holder.categotyImage = categoryView.findViewById(R.id.categoryImage)
+           holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        }else{
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+
+        val categoty = categories[position]
+
+        val resourceId = context.resources.getIdentifier(categoty.image, "drawable", context.packageName)
+        holder.categotyImage?.setImageResource(resourceId)
+       
+
+        holder.categoryName?.text = categoty.title
+          return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories [position]
+    }
+
+    override fun getItemId(position: Int): Long {
+       return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+    private class ViewHolder{
+        var categotyImage: ImageView? = null
+        var categoryName: TextView? = null
+    }
+}
--- app/src/main/java/com/nestergal/coderswag/Services/DataService.kt
+++ app/src/main/java/com/nestergal/coderswag/Services/DataService.kt
@@ -11,7 +11,7 @@ object DataService {
             Category("SHIRTS", "shirtimage"),
             Category ("HOODIES", "hoodieimage"),
             Category("HATS","hatimage"),
-            Category("DIGITAL","digitalgoodimage")
+            Category("DIGITAL","digitalgoodsimage")
     )
 
     val hats = listOf(
