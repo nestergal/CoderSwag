19b7217d926d830359e525fcb435bcd817c8faa9
nestergal <nestergal@gmail.com>
2018-01-06 13:38:12 +0200

    refactored for recycler view adapter

--- app/src/main/java/com/nestergal/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/nestergal/coderswag/Adapters/CategoryAdapter.kt
@@ -14,9 +14,7 @@ import com.nestergal.coderswag.R
 /**
  * Created by neste on 1/4/2018.
  */
-class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
-    val context = context
-    val categories = categories
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter() {
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
--- app/src/main/java/com/nestergal/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/nestergal/coderswag/Controller/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.nestergal.coderswag.Controller
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import com.nestergal.coderswag.Adapters.CategoryAdapter
+
+import com.nestergal.coderswag.Adapters.CategoryRecycleAdapter
 import com.nestergal.coderswag.Model.Category
 import com.nestergal.coderswag.R
 import com.nestergal.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter :CategoryAdapter
+    lateinit var adapter :CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
 
         categoryListView.adapter = adapter
+
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
+                //notification in case we use CategoryAdapter (not CategoryRecycleAdapter)
 //        categoryListView.setOnItemClickListener { parent, view, position, id ->
 //            val category = DataService.categories[position]
 //            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
